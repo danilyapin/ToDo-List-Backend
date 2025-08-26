9ce07a3ea43ff2ba531d577ddb4f4821f77802e5
Der Portugiese <lyapin.daniel@mm-bbs.de>
2025-08-27 01:30:33 +0200

    ToDoRepository erstllt, um Spring mit Datenbank zu verbinden

--- src/main/java/com/project/todo_list/Repository/ToDoRepository.java
+++ src/main/java/com/project/todo_list/Repository/ToDoRepository.java
@@ -3,5 +3,8 @@ package com.project.todo_list.Repository;
 import com.project.todo_list.Model.ToDo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ToDoRepository extends JpaRepository<ToDo, Long> {
+    List<ToDo> findByCompletedTrue();
 }
